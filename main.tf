4dd161deb23426b16a2dc0b6fe9ef0cd98a184f7
James Dumont <j.dumont@veebaze.com>
2020-02-10 09:27:55 +0100

    Suppress the deprecation warning about nsg and route links.

--- main.tf
+++ main.tf
@@ -52,13 +52,13 @@ resource "azurerm_virtual_network" "vnets" {
 # -
 
 resource "azurerm_subnet" "subnets" {
-  for_each                  = var.subnets
-  name                      = each.value["name"]
-  resource_group_name       = data.azurerm_resource_group.network.name
-  address_prefix            = each.value["address_prefix"]
-  service_endpoints         = lookup(each.value, "service_endpoints", null)
-  route_table_id            = lookup(each.value, "rt_key", null) == null ? null : lookup(azurerm_route_table.rts, each.value["rt_key"], null)["id"]
-  network_security_group_id = lookup(each.value, "nsg_key", null) == null ? null : lookup(azurerm_network_security_group.nsgs, each.value["nsg_key"], null)["id"]
+  for_each            = var.subnets
+  name                = each.value["name"]
+  resource_group_name = data.azurerm_resource_group.network.name
+  address_prefix      = each.value["address_prefix"]
+  service_endpoints   = lookup(each.value, "service_endpoints", null)
+  #route_table_id            = lookup(each.value, "rt_key", null) == null ? null : lookup(azurerm_route_table.rts, each.value["rt_key"], null)["id"]
+  #network_security_group_id = lookup(each.value, "nsg_key", null) == null ? null : lookup(azurerm_network_security_group.nsgs, each.value["nsg_key"], null)["id"]
 
   /*
   This forces a destroy when adding a new vnet --> 

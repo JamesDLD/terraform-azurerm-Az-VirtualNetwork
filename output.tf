a6f1cc3e44151158d9907b59e569e83181faaeac
James Dumont <j.dumont@veebaze.com>
2020-03-18 17:15:58 +0100

    Only output maps

--- output.tf
+++ output.tf
@@ -1,39 +1,4 @@
 # -
-# - Network - List outputs
-# -
-
-output "vnet_ids" {
-  value = [for x in azurerm_virtual_network.vnets : x.id]
-}
-
-output "vnet_names" {
-  value = [for x in azurerm_virtual_network.vnets : x.name]
-}
-
-output "vnet_locations" {
-  value = [for x in azurerm_virtual_network.vnets : x.location]
-}
-
-output "vnet_rgnames" {
-  value = [for x in azurerm_virtual_network.vnets : x.resource_group_name]
-}
-
-output "subnet_ids" {
-  value = [for x in azurerm_subnet.subnets : x.id]
-
-}
-output "network_security_group_ids" {
-  value = [for x in azurerm_network_security_group.nsgs : x.id]
-}
-output "route_table_ids" {
-  value = [for x in azurerm_route_table.rts : x.id]
-}
-
-output "public_ip_ids" {
-  value = [for x in azurerm_public_ip.pips : x.id]
-}
-
-# -
 # - Network - Map outputs
 # -
 
@@ -47,9 +12,23 @@ output "subnets" {
   value       = { for k, b in azurerm_subnet.subnets : k => b }
 }
 
+output "network_security_groups" {
+  description = "Map output of the Network Security Groups"
+  value       = { for k, b in azurerm_network_security_group.nsgs : k => b }
+}
+
+
+output "route_tables" {
+  description = "Map output of the Route Tables"
+  value       = { for k, b in azurerm_route_table.rts : k => b }
+}
+
+output "public_ips" {
+  description = "Map output of the Public Ips"
+  value       = { for k, b in azurerm_public_ip.pips : k => b }
+}
+
 output "private_endpoints" {
   description = "Map output of the private endpoints"
   value       = { for k, b in azurerm_private_endpoint.pend : k => b }
 }
-
-#data.azurerm_private_endpoint_connection.example.private_service_connection.0.status
